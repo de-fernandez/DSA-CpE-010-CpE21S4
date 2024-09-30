6230200f26f067728ec57dcf51eb119aae9131af
Don Eleazar T. Fernandez <qdetfernandez@tip.edu.ph>
2024-09-30 15:49:59 +0800

    Practical Exam

---
+++ Don Eleazar T. Fernandez - PracticalLabExam.cpp
@@ -0,0 +1,54 @@
+// Don Eleazar T. Fernandez
+// CPE21S4, CPE 010
+// Practical Exam
+
+// singly linked list
+// Given the set of element (a, b, c, d, e, f) stored in a list, show the final state of the list,
+// assuming we use the move-to-front heuristic and access the elements according to the following sequence:
+// (a, b, c, d, e, f, a, c, f, b, d, e).
+
+#include <iostream>
+using namespace std;
+
+class Node(){
+    int data;
+    Node -> new Node;
+}
+
+int main(){
+    int head;
+    int one = new Node;
+    int two = new Node;
+    int three = new Node;
+    int four = new Node;
+    int five = new Node;
+    int six = new Node;
+    
+    Node* one = NULL;
+    Node* two = NULL;
+    Node* three = NULL;
+    Node* four = NULL;
+    Node* five = NULL;
+    Node* six = NULL;
+    
+    head = Node;
+    Node* one = 'a';
+    Node* two = 'b';
+    Node* three = 'c';
+    Node* four = 'd';
+    Node* five = 'e';
+    Node* six = 'f';
+    
+    one -> two = Node;
+    two -> three = Node;
+    three -> four = Node;
+    four -> five = Node;
+    five -> six = NULL;
+    
+    while(!NULL){
+        head = data;
+        cout << head << endl;
+        head = head -> next;
+    }
+return 0;
+}
